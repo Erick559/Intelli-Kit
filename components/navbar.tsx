88877d2ec0f12e4e467ce2d0a25142a0836d2706
Erick559 <erickungu82@gmail.com>
2024-02-08 21:59:29 +0300

    Add a hamburger menu icon from radix ui.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,13 @@
+import { DropdownMenuIcon, HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
 
 const Navbar = () => {
     return ( 
-        <div className="flext items-center p-4"></div>
+        <div className="flext items-center p-4">
+            <Button variant="ghost" size="icon" className="md:hidden">
+                <HamburgerMenuIcon />
+            </Button>
+        </div>
      );
 }
  
