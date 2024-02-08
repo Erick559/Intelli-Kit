9a78be50797cd7a1d9f41f22ae47d1fe45b0bd32
Erick559 <erickungu82@gmail.com>
2024-02-08 21:31:10 +0300

    Create the dashboard layout file.

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,13 @@
+const DashboardLayout = ({
+    children
+}:{
+    children:React.ReactNode;
+}) => {
+    return ( 
+        <div className="h-full relative">
+
+        </div>
+     );
+}
+ 
+export default DashboardLayout ;
