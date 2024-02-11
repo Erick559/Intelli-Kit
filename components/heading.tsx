5ae74c26d28cbfb6972085ebea5a4e63eafa4a84
Erick559 <erickungu82@gmail.com>
2024-02-11 21:26:41 +0300

    Fix indentation.

--- components/heading.tsx
+++ components/heading.tsx
@@ -14,21 +14,19 @@ const Heading = ({
     iconColor
 }:HeadingProps) => {
     return ( 
-        <>
-            <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
-                <div className={cn("p-2 2-fit rounded-md")}>
-                    <Icon className={cn("w-8 h-8", iconColor)} />
-                </div>
-                <div>
-                    <h2 className="text-2xl font-bold bg-gradient-to-r from-violet-400 via-red-400 to-red-500 inline-block text-transparent bg-clip-text">
-                        {title}    
-                    </h2>                
+        <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
+            <div className={cn("p-2 2-fit rounded-md")}>
+                <Icon className={cn("w-8 h-8", iconColor)} />
+            </div>
+            <div>
+                <h2 className="text-2xl font-bold bg-gradient-to-r from-violet-400 via-red-400 to-red-500 inline-block text-transparent bg-clip-text">
+                    {title}    
+                </h2> 
                 <p className="text-sm text-mute-foreground">
                     {description} 
                 </p>               
-                </div>
             </div>
-        </>
+        </div>
      );
 }
  
