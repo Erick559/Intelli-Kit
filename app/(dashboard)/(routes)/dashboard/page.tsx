8fdc3488fae0de1b25a68c305c440d0b9569a075
Erick559 <erickungu82@gmail.com>
2024-02-11 20:37:50 +0300

    Display and style the tools in their individual cards.

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -54,7 +54,20 @@ const DashboardPage = () => {
           </p>
         </div>
         <div className="px-4 md:px-20 lg:px-32 space-y-4">
-
+          {tools.map((tool)=> (
+            <Card 
+              key={tool.href}
+              className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
+            >
+              <div className="flex items-center gap-x-4">
+                  <tool.icon className={cn("w-8 h-8", tool.color)} />
+                <div className="font-semibold">
+                    {tool.label}
+                  </div>
+              </div>
+              <ArrowRightIcon className="w-5 h-5"/>
+            </Card>
+          ))}
         </div>
       </div>
         
