818eff81349e3790c58f5ec74761d830d319a28c
Erick559 <erickungu82@gmail.com>
2024-02-11 20:37:13 +0300

    Add the tools in an array that will be used to iterate over them and display them in their individual card.

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,47 @@
 "use client"
 
+import { Card } from "@/components/ui/card"
+import { cn } from "@/lib/utils"
+
+import { ArrowRightIcon, ChatBubbleIcon, CodeIcon, ImageIcon, SpeakerLoudIcon, VideoIcon } from "@radix-ui/react-icons"
+
+const tools= [
+  {
+    label:"Conversation",
+    icon: ChatBubbleIcon,
+    color:"text-rose-500",
+    href:"/conversation"
+  },
+
+  {
+    label:"Image Generator",
+    icon: ImageIcon,
+    color:"text-indigo-400",
+    href:"/conversation"
+  },
+
+  {
+    label:"Video Generator",
+    icon: VideoIcon,
+    color:"text-orange-500",
+    href:"/conversation"
+  },
+
+  {
+    label:"Music Generator",
+    icon: SpeakerLoudIcon,
+    color:"text-lime-400",
+    href:"/conversation"
+  },
+
+  {
+    label:"Code Generator",
+    icon: CodeIcon,
+    color:"text-yellow-500",
+    href:"/conversation"
+  }
+]
+
 const DashboardPage = () => {
     return (
       <div>
