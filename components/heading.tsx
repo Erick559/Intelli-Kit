09acacb9836984dec1b30afcc8027425ce2ed9c1
Erick559 <erickungu82@gmail.com>
2024-02-11 21:16:10 +0300

    Render the tool title.

--- components/heading.tsx
+++ components/heading.tsx
@@ -19,6 +19,11 @@ const Heading = ({
                 <div className={cn("p-2 2-fit rounded-md")}>
                     <Icon className={cn("w-8 h-8", iconColor)} />
                 </div>
+                <div>
+                    <h2 className="text-2xl font-bold bg-gradient-to-r from-violet-400 via-red-400 to-red-500 inline-block text-transparent bg-clip-text">
+                        {title}    
+                    </h2>                
+                </div>
             </div>
         </>
      );
