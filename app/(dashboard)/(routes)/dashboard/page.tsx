d120b9684e3a5896779811efc7c9a696b2aee702
Erick559 <erickungu82@gmail.com>
2024-02-11 20:42:53 +0300

    Add a click function to the cards to redirect to the tools respective pages.

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { Card } from "@/components/ui/card"
 import { cn } from "@/lib/utils"
 
 import { ArrowRightIcon, ChatBubbleIcon, CodeIcon, ImageIcon, SpeakerLoudIcon, VideoIcon } from "@radix-ui/react-icons"
+import { useRouter } from "next/navigation"
 
 const tools= [
   {
@@ -43,6 +44,7 @@ const tools= [
 ]
 
 const DashboardPage = () => {
+  const router = useRouter()
     return (
       <div>
         <div className="mb-8 space-y-4">
@@ -56,6 +58,7 @@ const DashboardPage = () => {
         <div className="px-4 md:px-20 lg:px-32 space-y-4">
           {tools.map((tool)=> (
             <Card 
+            onClick={()=> router.push(tool.href)}
               key={tool.href}
               className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
             >
