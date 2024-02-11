f0bce3a5f87990e4a6c50e338a6daa11f5b0fb12
Erick559 <erickungu82@gmail.com>
2024-02-11 19:54:26 +0300

    Add an introductory title to the dashboard.

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,7 +1,17 @@
 const DashboardPage = () => {
     return (
       <div>
-        <p>Dashboard Page (Protected)</p>
+        <div className="mb-8 space-y-4">
+          <h2 className="text-2xl md:text4xl font-bold text-center">
+            Welcome to IntelliKit, a Trusted AI Platform.
+          </h2>
+          <p className="text-muted-foreground font-light text-sm md:text:lg text-center">
+            Harness the capabilities of AI with our provided tools.
+          </p>
+        </div>
+        <div className="px-4 md:px-20 lg:px-32 space-y-4">
+          
+        </div>
       </div>
         
       )
