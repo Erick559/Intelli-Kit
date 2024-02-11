ab3941a1c7a6c50d8cf12525540de19f53cd7b7c
Erick559 <erickungu82@gmail.com>
2024-02-11 21:11:53 +0300

    Wrap the container div.

--- components/heading.tsx
+++ components/heading.tsx
@@ -14,11 +14,13 @@ const Heading = ({
     iconColor
 }:HeadingProps) => {
     return ( 
-        <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
-            <div className={cn("p-2 2-fit rounded-md")}>
-                <Icon className={cn("w-8 h-8", iconColor)} />
+        <>
+            <div className="px-4 lg:px-8 flex items-center gap-x-3 mb-8">
+                <div className={cn("p-2 2-fit rounded-md")}>
+                    <Icon className={cn("w-8 h-8", iconColor)} />
+                </div>
             </div>
-        </div>
+        </>
      );
 }
  
