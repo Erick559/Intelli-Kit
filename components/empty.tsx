8efd5d8d1db6511b9ea3642ba253c0d7d6b47d14
Erick559 <erickungu82@gmail.com>
2024-02-14 20:58:03 +0300

    Created an empty component that renders an image and a label that acts as a placeholder.

--- components/empty.tsx
+++ components/empty.tsx
@@ -1,7 +1,25 @@
-const Empty = () => {
+import Image from "next/image";
+
+interface EmptyProps{
+    label:string
+}
+
+const Empty = ({
+    label
+}: EmptyProps)=> {
     return (
-        <div>
-            Empty Component
+        <div className="h-full p-20 flex flex-col items-center justify-center">
+             <div className="relative h-80 w-80">
+                <Image 
+                    alt="Empty Field"
+                    fill
+                    src="/Tablet.png"
+                />
+             </div>
+
+             <p className="text-muted-foreground text-sm text-center">
+                {label}
+             </p>
         </div>
     );
 }
