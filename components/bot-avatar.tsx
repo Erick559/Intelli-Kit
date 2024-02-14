859001f1abe379e2aa6eb3a842ba05869444bc37
Erick559 <erickungu82@gmail.com>
2024-02-14 21:35:38 +0300

    Create component bot-avatar to act as the AI models avatar.

---
+++ components/bot-avatar.tsx
@@ -0,0 +1,13 @@
+import { Avatar, AvatarImage } from "./ui/avatar";
+
+const BotAvatar = () => {
+    return (
+        <Avatar className="h-8 w-8">
+            <AvatarImage className="p-1" src ="/Logo.png">
+                
+            </AvatarImage>
+        </Avatar>
+    );
+}
+ 
+export default BotAvatar;
