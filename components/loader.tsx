5597e2b3eb17c2b4a486b7580eab10c16739d82a
Erick559 <erickungu82@gmail.com>
2024-02-14 21:12:56 +0300

    Add a phrase a spinning animation to the logo for the loading state.

---
+++ components/loader.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+const Loader = () => {
+    return (
+        <div className="h-full flex flex-col gap-y-4 items-center">
+            <div className="w-10 h-10 relative animate-spin">
+                <Image 
+                    alt='loader'
+                    fill
+                    src="/Logo.png"
+                />
+            </div>
+            <p className="text-sm text-muted-foreground">
+                Crafting intelligence, please await a moment..."
+            </p>
+        </div>
+    );
+}
+ 
+export default Loader;
