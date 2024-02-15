68fd2a2146116f2f236e24715724c19b9711072c
Erick559 <erickungu82@gmail.com>
2024-02-15 19:01:16 +0300

    Modify the rendered response to be in markdown to accomodate code copying and readability.

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -126,9 +126,21 @@ const CodePage = () => {
                             )}
                         >
                             {message.role === 'user' ? <UserAvatar /> : <BotAvatar />}
-                            <p className="text-sm">
-                                {message.content}
-                            </p>
+                            <ReactMarkdown
+                                components={{
+                                    pre: ({node, ...props}) => (
+                                        <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                            <pre {...props}/>
+                                        </div>
+                                    ),
+                                    code: ({node, ...props}) => (
+                                        <code className="bg-black/10 p-2 rounded-lg" {...props}/>
+                                    )
+                                }}
+                                className="text-sm overflow-hidden leading-7"
+                            >
+                                {message.content || ""}
+                            </ReactMarkdown>
                         </div>
                     ))}
                    </div>
