903b1fdf36053663214843d9bad56ff4fe7df2a1
Erick559 <erickungu82@gmail.com>
2024-02-15 18:35:14 +0300

    Change the api directory.

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -41,7 +41,7 @@ const CodePage = () => {
             const newMessages = [...messages, userMessage];
 
 
-            const response = await axios.post("/api/conversation", {messages:newMessages})
+            const response = await axios.post("/api/code", {messages:newMessages})
             setMessages((current)=> [...current, userMessage, response.data]);
 
             form.reset();
