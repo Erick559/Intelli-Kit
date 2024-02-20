ac240478d55bb8173d57424fa36a961f8a12564a
Erick559 <erickungu82@gmail.com>
2024-02-20 20:06:39 +0300

    Retrieve the response images urls and set it to SetImages.

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -38,7 +38,11 @@ const ImagePage = () => {
         try {
             setImages([]);
 
-            const response = await axios.post("/api/image");
+            const response = await axios.post("/api/image", values);
+
+            const urls = response.data.map((image: {url: string}) => image.url)
+
+            setImages(urls);
 
             form.reset();
 
