e236716f9c5aafcd828d5080672f16b322930d77
Erick559 <erickungu82@gmail.com>
2024-02-20 19:37:06 +0300

    Add amount and resolution props allowing the user to select the number of images to generate and resolution.

--- app/(dashboard)/(routes)/image/constants.ts
+++ app/(dashboard)/(routes)/image/constants.ts
@@ -3,5 +3,7 @@ import * as z from "zod"
 export const formSchema = z.object({
     prompt: z.string().min(1,{
         message: "Please enter your Image prompt",
-    })
+    }),
+    amount:z.string().min(1),
+    resolution:z.string().min(1)
 })
