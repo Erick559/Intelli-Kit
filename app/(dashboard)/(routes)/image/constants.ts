15a064959b92a4996a739d47574d714662fa04ef
Erick559 <erickungu82@gmail.com>
2024-02-20 19:30:21 +0300

    Change the prompt message.

--- app/(dashboard)/(routes)/image/constants.ts
+++ app/(dashboard)/(routes)/image/constants.ts
@@ -2,6 +2,6 @@ import * as z from "zod"
 
 export const formSchema = z.object({
     prompt: z.string().min(1,{
-        message: "Please enter your prompt",
+        message: "Please enter your Image prompt",
     })
 })
