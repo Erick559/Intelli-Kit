6868df085407b29a828a36005bdbe16da16de922
Erick559 <erickungu82@gmail.com>
2024-03-18 14:01:34 +0300

    feat: Intergrate the free generation count for the mobile sidebar.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,11 +2,13 @@ import { DropdownMenuIcon, HamburgerMenuIcon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
 import { UserButton } from "@clerk/nextjs";
 import MobileSidebar from "./mobile-sidebar";
+import { getApiLimitCount } from "@/lib/api_limits";
 
-const Navbar = () => {
+const Navbar = async() => {
+    const apiLimitCount = await getApiLimitCount()
     return ( 
         <div className="flext items-center p-4">
-            <MobileSidebar />
+            <MobileSidebar apiLimitCount = {apiLimitCount}/>
             
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/"/>
