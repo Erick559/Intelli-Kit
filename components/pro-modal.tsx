8c27c601bd2e091b1ee92467b007d45de8af46e3
Erick559 <erickungu82@gmail.com>
2024-03-19 15:42:24 +0300

    feat: Intergrate Stripe payment method.

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -8,10 +8,27 @@ import { Card } from "./ui/card"
 import { cn } from "@/lib/utils"
 import { CheckIcon, LightningBoltIcon } from "@radix-ui/react-icons"
 import { Button } from "./ui/button"
+import axios from "axios"
+import { useState } from "react"
 
 export const ProModal = () => {
     const proModal = useProModal()
     const tools = tool
+    const [loading,setLoading] = useState(false);
+
+    const onSubscribe = async() => {
+        try {
+            setLoading(true);
+            const response = await axios.get('/api/stripe');
+
+            window.location.href = response.data.url;
+
+        } catch (error) {
+            console.log(error, "STRIPE_CLIENT_ERROR")
+        }finally{
+            setLoading(false);
+        }
+    }
 
     return(
         <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -43,7 +60,7 @@ export const ProModal = () => {
                     </DialogDescription>
                 </DialogHeader>
                 <DialogFooter>
-                    <Button size={"lg"} className='w-full' variant={'premium'}>
+                    <Button onClick={onSubscribe} size={"lg"} className='w-full' variant={'premium'}>
                             Upgrade
                             <LightningBoltIcon className='w-4 h-4 ml-2' />
                     </Button>
