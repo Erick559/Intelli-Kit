7bd21d36c8bda8f7099e4991c41adc2bffba0c92
Erick559 <erickungu82@gmail.com>
2024-03-19 17:18:38 +0300

    feat: Create the settings page.

---
+++ app/(dashboard)/(routes)/settings/page.tsx
@@ -0,0 +1,30 @@
+import Heading from "@/components/heading";
+import SubscriptionButton from "@/components/subscription-button";
+import { checkSubscription } from "@/lib/subscription_check";
+import { GearIcon } from "@radix-ui/react-icons";
+import React from "react"
+
+const Settings = async () => {
+    const isPro = await checkSubscription();
+  return (
+    <div>
+      <Heading 
+        title="Settings"
+        description="Configure your account settings."
+        icon={GearIcon}
+        iconColor="text-gray-700"
+      /> 
+
+      <div className="px-4 lg:px-8 space-y-4">
+        <div className="text-muted-foreground text-sm">
+            {isPro ? "You are currently subscribed to Mimir Pro": "You are not subscribed to Mimir Pro"}
+        </div>
+        <SubscriptionButton isPro={isPro}/>
+      </div>
+
+      
+    </div>
+  )
+};
+
+export default Settings;
