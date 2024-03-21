11babe42ebfc35c514e14bbd66091ab9b1483f50
Erick559 <erickungu82@gmail.com>
2024-03-21 12:17:12 +0300

    feat: Create the header section of the landing page.

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -7,10 +7,8 @@ import Link from "next/link";
 const LandingPage = () => {
     return (  
         <>
-            <div className="relative h-full w-full bg-white">
-                <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">
-                   
-                </div>
+            <div className="h-full pt-[4.75rem] lg:-[5.25rem] overflow-hidden bg-n-8 text-white">
+                <NavigationBar />
             </div>
         </>
     );
--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -1,56 +1,73 @@
-import { Poppins } from "next/font/google";
+'use client'
 
 import Image from "next/image";
 import Link from "next/link";
 import { useAuth, UserButton } from "@clerk/nextjs";
-import { Button } from "./ui/button";
-import UserAvatar from "./user-avatar";
+import { Button} from "./ui/button";
 import { cn } from "@/lib/utils";
-
-const poppins = Poppins ({
-    weight:['500','600','700'],
-    subsets: ['latin'],
-
-})
+import { navigation } from "@/constants";
+import { usePathname } from "next/navigation";
+import SecondaryButton from "./ui/secondary-button"
+import { useState } from "react";
 
 const NavigationBar = () => {
     const { isSignedIn} = useAuth();
+    const pathName = usePathname();
+    const [loading,setLoading] = useState(false);
     return (
-        <div className="p-4 flex justify-between items-center relative z-10">
-            <aside className="flex items-center gap-1">
-                <Image 
-                    src={'/Logo.png'}
-                    width={60}
-                    height={60}
-                    alt="Mimir Logo"
-                />
-                <span className="text-xl font-bold">
-                    Mimir.
-                </span>
-            </aside>
-
-            <nav className="hidden md:block">
-                <ul className="flex items-center justify-center gap-8">
-                    <Link href={'#'}>Features</Link>
-                    <Link href={'#'}>Pricing</Link>
-                </ul>
-            </nav>
-
-          
-            <aside className="flex items-center justify-center gap-7">
-                <Link
-                    href={isSignedIn ? '/dashboard':'/sign-up'}
-                >
-                    <Button variant={'premium'} className={poppins.className}>
-                        TRY FOR FREE
-                    </Button>
+        <div className="fixed top-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+           <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4 ">
+                <Link className="flex items-center" href='#hero'>
+                    <Image 
+                        src='/Logo.png'
+                        alt="Mimir Logo"
+                        width={60}
+                        height={60}
+                    />
+                    <span className="text-3xl font-bold">
+                        Mimir.
+                    </span>
                 </Link>
 
-                <UserButton />
-            </aside>
+                <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+                    <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+                       {navigation.map((item) => {
+                            const isActive = pathName.startsWith(item.href);
+                            return(
+                                <Link 
+                                    className={`block relative font-code text-xl uppercase transition-colors hover:text-color-1 hover:underline ${item.onlyMobile ? 'lg:hidden': ''} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold lg:leading-5 xl:px-10`}
+                                    key={item.id}
+                                    href = {item.href}
+                                >
+                                    {item.title}                            
+                                </Link>
+                            )
+                          
+                        })}
+                    </div>
+                </nav>
 
+                <div className="flex items-center gap-5">
+                    <Link
+                        className="hidden lg:block"
+                        href = {isSignedIn ? '/dashboard' : '/sign-up'}
+                    >
+                        <Button className="uppercase" variant={'premium'}>try for free</Button>
+                    </Link>
+
+                    <Link href={isSignedIn ? '/dashboard' : '/sign-up'}>
+                        <SecondaryButton className="px-8" loading = {loading}>
+                            Sign In
+                        </SecondaryButton>
+                    </Link>
+                </div>
+           </div>
         </div>
     )
 }
 
-export default NavigationBar
+export default NavigationBar;
+
+
+
+
