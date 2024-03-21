39cc38f6996a934336b0ddf859a84cbc7b91b98d
Erick559 <erickungu82@gmail.com>
2024-03-21 08:29:17 +0300

    fix:

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,32 @@ const config = {
     },
     extend: {
       colors: {
+        color:{
+          1: "#AC6AFF",
+          2: "#FFC876",
+          3: "#FF776F",
+          4: "#7ADB78",
+          5: "#858DFF",
+          6: "#FF98E2",
+        },
+        stroke:{
+          1: "#26242C",
+        },
+        n:{
+          1: "#FFFFFF",
+          2: "#CAC6DD",
+          3: "#ADA8C3",
+          4: "#757185",
+          5: "#3F3A52",
+          6: "#252134",
+          7: "#15131D",
+          8: "#0E0C15",
+          9: "#474060",
+          10: "#43435C",
+          11: "#1B1B2E",
+          12: "#2E2A41",
+          13: "#6C7275",
+        },
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -53,6 +79,37 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      fontFamily: {
+        code: "var(--font-code)",
+        grotesk: "var(--font-grotesk)",
+      },
+      letterSpacing: {
+        tagline: ".15em",
+      },
+      spacing: {
+        0.25: "0.0625rem",
+        7.5: "1.875rem",
+        15: "3.75rem",
+      },
+      opacity: {
+        15: ".15",
+      },
+      transitionDuration: {
+        DEFAULT: "200ms",
+      },
+      transitionTimingFunction: {
+        DEFAULT: "linear",
+      },
+      zIndex: {
+        1: "1",
+        2: "2",
+        3: "3",
+        4: "4",
+        5: "5",
+      },
+      borderWidth: {
+        DEFAULT: "0.0625rem",
+      },     
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
