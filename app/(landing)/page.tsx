542abc76b886abafb0d233b6560b9f256c94203b
Erick559 <erickungu82@gmail.com>
2024-03-21 18:53:39 +0300

    feat: Render the hero page.

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Hero from "@/components/hero";
 import NavigationBar from "@/components/navigation-bar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
@@ -9,6 +10,7 @@ const LandingPage = () => {
         <>
             <div className="h-full pt-[4.75rem] lg:-[5.25rem] overflow-hidden bg-n-8 text-white">
                 <NavigationBar />
+                <Hero />
             </div>
         </>
     );
