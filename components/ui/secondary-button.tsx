7936110519343e2d010ec5b61438ea3123c3b0fb
Erick559 <erickungu82@gmail.com>
2024-03-21 15:58:04 +0300

    fix;

--- components/ui/secondary-button.tsx
+++ components/ui/secondary-button.tsx
@@ -1,11 +1,9 @@
 'use client';
 import { cn } from "@/lib/utils";
 
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
-    loading: boolean;
-};
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> 
 
-const SecondaryButton = ({className,children,loading,...rest}: Props) => {
+const SecondaryButton = ({className,children,...rest}: Props) => {
     return (
         <button className={cn("relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50")}>
             <span className={cn("absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]")} />
