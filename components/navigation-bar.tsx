d81db0e44765a85326b16ff09c858031966a52a0
Erick559 <erickungu82@gmail.com>
2024-03-21 15:57:49 +0300

    feat: Create a hamburger navigation bar for small devices.

--- components/navigation-bar.tsx
+++ components/navigation-bar.tsx
@@ -8,14 +8,30 @@ import { cn } from "@/lib/utils";
 import { navigation } from "@/constants";
 import { usePathname } from "next/navigation";
 import SecondaryButton from "./ui/secondary-button"
+import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
+import MenuSvg from "./ui/MenuSvg";
 
 const NavigationBar = () => {
     const { isSignedIn} = useAuth();
     const pathName = usePathname();
-    const [loading,setLoading] = useState(false);
+    const [openNavigation,setOpenNavigation] = useState(false);
+    const toggleNavigation = () => {
+        if (openNavigation) {
+            setOpenNavigation(false)
+        }
+        else{
+            setOpenNavigation(true);
+        }
+    }
+
+    const handleClick = () => {
+        setOpenNavigation(false);
+    }
+
+
     return (
-        <div className="fixed top-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+        <div className={`fixed top-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? 'bg-n-8':'bg-n-8/90 lg:backdrop-blur-sm'}`}>
            <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4 ">
                 <Link className="flex items-center" href='#hero'>
                     <Image 
@@ -29,7 +45,7 @@ const NavigationBar = () => {
                     </span>
                 </Link>
 
-                <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+                <nav className={`${openNavigation ? 'flex':'hidden'} fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}>
                     <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
                        {navigation.map((item) => {
                             const isActive = pathName.startsWith(item.href);
@@ -38,6 +54,7 @@ const NavigationBar = () => {
                                     className={`block relative font-code text-xl uppercase transition-colors hover:text-color-1 hover:underline ${item.onlyMobile ? 'lg:hidden': ''} px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold lg:leading-5 xl:px-10`}
                                     key={item.id}
                                     href = {item.href}
+                                    onClick={handleClick}
                                 >
                                     {item.title}                            
                                 </Link>
@@ -45,6 +62,7 @@ const NavigationBar = () => {
                           
                         })}
                     </div>
+                    <HamburgerMenu />
                 </nav>
 
                 <div className="flex items-center gap-5">
@@ -55,12 +73,15 @@ const NavigationBar = () => {
                         <Button className="uppercase" variant={'premium'}>try for free</Button>
                     </Link>
 
-                    <Link href={isSignedIn ? '/dashboard' : '/sign-up'}>
-                        <SecondaryButton className="px-8" loading = {loading}>
+                    <Link className="hidden lg:block" href={isSignedIn ? '/dashboard' : '/sign-in'}>
+                        <SecondaryButton className="px-8">
                             Sign In
                         </SecondaryButton>
                     </Link>
                 </div>
+                <Button variant={'premium'} className="ml-auto lg:hidden" onClick={toggleNavigation}>
+                        <MenuSvg openNavigation={openNavigation}/>
+                </Button>
            </div>
         </div>
     )
