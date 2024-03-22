5dff1f30ff5770822b9e82e15565d2c7723e823f
Erick559 <erickungu82@gmail.com>
2024-03-22 12:02:23 +0300

    feat: Add features constants to be displayed on the features section.

--- constants.ts
+++ constants.ts
@@ -28,7 +28,7 @@ export const navigation = [
 
 export const heroIcons = ['/chat.svg', '/code.svg', '/camera.svg', '/video.svg','/speaker.svg'];
 
-export const notificationImages = ['/image-1.png', '/image-2.png', '/image-3.png', '/image-4.png',]
+export const notificationImages = ['/user-1.png', '/user-2.png', '/user-3.png', '/user-4.png',]
 
 export const pricing = [
     {
@@ -65,3 +65,56 @@ export const pricing = [
       ],
     },
   ]
+
+  export const benefits = [
+    {
+      id: "0",
+      title: "Effortless Conversation",
+      text: "Mimir's AI Chatbot lets you ask anything and get instant, informative answers. Skip the endless searches and get right to the point.",
+      backgroundUrl: "/card-1.svg",
+      iconUrl: '/chat.svg', 
+      imageUrl: '/card-background.png',
+    },
+    {
+      id: "1",
+      title: "Streamline Coding",
+      text: "Mimir's code generation tool understands natural language. Describe your functionality and watch Mimir write clean, efficient code for you. Save time and avoid coding roadblocks.",
+      backgroundUrl: "/card-2.svg",
+      iconUrl: '/code.svg', 
+      imageUrl: '/card-background.png',
+      light: true,
+    },
+    {
+      id: "2",
+      title: "Compose Inspiring Music",
+      text: "Spark your creativity with Mimir's music generation. Describe the mood or genre you envision, and let Mimir generate unique instrumentals to fuel your musical projects.",
+      backgroundUrl: "/card-3.svg",
+      iconUrl: '/speaker.svg', 
+      imageUrl: '/card-background.png',
+    },
+    {
+      id: "3",
+      title: "Bring Ideas to Life with Video",
+      text: "Mimir's video generation tool transforms your vision into reality. Describe your concept, and Mimir will create a captivating video without the need for expensive equipment or editing software.",
+      backgroundUrl: "/card-4.svg",
+      iconUrl: '/video.svg', 
+      imageUrl: '/card-background.png',
+      light: true,
+    },
+    {
+      id: "4",
+      title: "Craft Stunning Photos",
+      text: "Go beyond imagination with Mimir's photo generation. Describe the scene you have in mind, and Mimir will generate high-quality, realistic photos to perfectly capture your vision.",
+      backgroundUrl: "/card-5.svg",
+      iconUrl: '/camera.svg', 
+      imageUrl: '/card-background.png',
+    },
+    {
+      id: "5",
+      title: "Continuously Evolving",
+      text: "Mimir's AI is constantly learning and improving. With every interaction, Mimir gets better at understanding your needs and generating exceptional results.",
+      backgroundUrl: "/card-6.svg",
+      iconUrl: '/ai.svg', 
+      imageUrl: '/card-background.png',
+    },
+  ];
