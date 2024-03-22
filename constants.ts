996c29af6fbefce3c5e4ad56e340d5fc01443921
Erick559 <erickungu82@gmail.com>
2024-03-22 13:54:59 +0300

    feat: Add a unique background image for each card.

--- constants.ts
+++ constants.ts
@@ -73,7 +73,7 @@ export const pricing = [
       text: "Mimir's AI Chatbot lets you ask anything and get instant, informative answers. Skip the endless searches and get right to the point.",
       backgroundUrl: "/card-1.svg",
       iconUrl: '/chat.svg', 
-      imageUrl: '/card-background.png',
+      imageUrl: '/tablet.png',
     },
     {
       id: "1",
@@ -81,7 +81,7 @@ export const pricing = [
       text: "Mimir's code generation tool understands natural language. Describe your functionality and watch Mimir write clean, efficient code for you. Save time and avoid coding roadblocks.",
       backgroundUrl: "/card-2.svg",
       iconUrl: '/code.svg', 
-      imageUrl: '/card-background.png',
+      imageUrl: '/developer.png',
       light: true,
     },
     {
@@ -90,7 +90,7 @@ export const pricing = [
       text: "Spark your creativity with Mimir's music generation. Describe the mood or genre you envision, and let Mimir generate unique instrumentals to fuel your musical projects.",
       backgroundUrl: "/card-3.svg",
       iconUrl: '/speaker.svg', 
-      imageUrl: '/card-background.png',
+      imageUrl: '/music.png',
     },
     {
       id: "3",
@@ -98,7 +98,7 @@ export const pricing = [
       text: "Mimir's video generation tool transforms your vision into reality. Describe your concept, and Mimir will create a captivating video without the need for expensive equipment or editing software.",
       backgroundUrl: "/card-4.svg",
       iconUrl: '/video.svg', 
-      imageUrl: '/card-background.png',
+      imageUrl: '/video.png',
       light: true,
     },
     {
@@ -107,7 +107,7 @@ export const pricing = [
       text: "Go beyond imagination with Mimir's photo generation. Describe the scene you have in mind, and Mimir will generate high-quality, realistic photos to perfectly capture your vision.",
       backgroundUrl: "/card-5.svg",
       iconUrl: '/camera.svg', 
-      imageUrl: '/card-background.png',
+      imageUrl: '/image.png',
     },
     {
       id: "5",
@@ -115,6 +115,6 @@ export const pricing = [
       text: "Mimir's AI is constantly learning and improving. With every interaction, Mimir gets better at understanding your needs and generating exceptional results.",
       backgroundUrl: "/card-6.svg",
       iconUrl: '/ai.svg', 
-      imageUrl: '/card-background.png',
+      imageUrl: '/neural.png',
     },
   ];
