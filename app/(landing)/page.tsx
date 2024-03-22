af96c7d3e9f7ce465747c550bb90e3cba1dd66c8
Erick559 <erickungu82@gmail.com>
2024-03-22 10:36:14 +0300

    fix: Remove implicit height declaration.

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 const LandingPage = () => {
     return (  
         <>
-            <div className="h-full pt-[4.75rem] lg:-[5.25rem] overflow-hidden bg-n-8 text-white">
+            <div className="pt-[4.75rem] lg:-[5.25rem] overflow-hidden bg-n-8 text-white">
                 <NavigationBar />
                 <Hero />
             </div>
