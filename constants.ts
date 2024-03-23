4845308354ebd6273e68e02808532b7b7759e5ae
Erick559 <erickungu82@gmail.com>
2024-03-23 12:22:22 +0300

    Constant for the footer social links.

--- constants.ts
+++ constants.ts
@@ -121,3 +121,36 @@ export const pricing = [
       imageUrl: '/neural.png',
     },
   ];
+
+  export const socials = [
+    {
+      id: "0",
+      title: "Discord",
+      iconUrl: '/discord.svg',
+      url: "#",
+    },
+    {
+      id: "1",
+      title: "Twitter",
+      iconUrl: '/twitter.svg',
+      url: "#",
+    },
+    {
+      id: "2",
+      title: "Instagram",
+      iconUrl: '/instagram.svg',
+      url: "#",
+    },
+    {
+      id: "3",
+      title: "Telegram",
+      iconUrl: '/telegram.svg',
+      url: "#",
+    },
+    {
+      id: "4",
+      title: "Facebook",
+      iconUrl: '/facebook.svg',
+      url: "#",
+    },
+  ];
