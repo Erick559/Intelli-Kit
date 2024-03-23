6afd2607cfdeeb6a57bfd88bf7fbcbbc20caf021
Erick559 <erickungu82@gmail.com>
2024-03-23 09:12:42 +0300

    fix:

--- components/landing-page/Tagline.tsx
+++ components/landing-page/Tagline.tsx
@@ -4,7 +4,7 @@ const Tagline = ({children,className}) => {
   return (
     <div className={`font-grotesk font-light text-xs tracking-tagline uppercase flex items-center ${className || ''}`}>
       {brackets('left')}
-      <div className="mx-3 mb-1 text-n-3 ">
+      <div className="mx-3  text-n-3 ">
             {children}
       </div>
       {brackets('right')}
